61a79d3f6a86d43257d6f44fe361602bd956deb2
Vinícius Pereira <vinicius.pereira23@hotmail.com>
2023-07-13 14:46:53 -0300

    beta 1.0

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Generator CSS
+- Um gerador de CSS que cria um botão com opções selecionadas visualmente, baseado em um mini sistema no-code.
+
+# Conceitos Aplicados
+- Pegar o atributo `name` como propriedade de um elemento `event.target.name;`
+- Pegar o atributo `value` ou o seu valor futuro definido pelo usuário como propriedade de um elemento `event.target.value;`
+- Uso do `elements`, propriedade do `document.forms`.
+- Uso do `cssText`, propriedade do `CSSStyleDeclaration`.
+- Objeto com métodos com o mesmo nome do atributo `name`.
+- Objeto passado dentro de uma função de forma totalmente dinâmica: 
+  ```const name = event.target.name;
+     const value = event.target.value;
+     handleStyle[name](value);```
+- Conceito de localStorage para salvar preferências do usuário diretamente no navegador:
+```const properties = Object.keys(localStorage); // array com keys
+
+  properties.forEach(prop => {
+    handleStyle[prop](localStorage[prop]);
+    controles.elements[prop].value = localStorage[prop];
+  });```
---
+++ script.js
@@ -0,0 +1,64 @@
+const controles = document.getElementById('controles');
+const css = document.querySelector('.css');
+const btn = document.querySelector('.btn');
+
+controles.addEventListener('change', handleChange);
+
+const handleStyle = {
+  element: btn,
+  texto(value) {
+    this.element.innerText = value;
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + 'px';
+  },
+  width(value) {
+    this.element.style.width = value + 'px';
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + 'px';
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + 'px';
+  },
+}
+
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+  handleStyle[name](value);
+  showCss();
+  saveValues(name, value);
+}
+
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage); // array com keys
+
+  properties.forEach(prop => {
+    handleStyle[prop](localStorage[prop]);
+    controles.elements[prop].value = localStorage[prop];
+  });
+  showCss();
+}
+setValues();
+
+function showCss() {
+  css.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>')
+}
+
