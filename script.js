49a6c704360f4a4dbe07d02cbf7d08d79b695caa
Vinícius Pereira <vinicius.pereira23@hotmail.com>
2023-07-13 16:29:52 -0300

    add button reset styles

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const controles = document.getElementById('controles');
 const css = document.querySelector('.css');
 const btn = document.querySelector('.btn');
+const reset = document.querySelector('.reset');
 
 controles.addEventListener('input', handleChange);
+reset.addEventListener('click', resetValues);
+
+const defaultStyles = {
+  color: '', // Estilo padrão de cor
+  backgroundColor: '', // Estilo padrão de cor de fundo
+  height: '', // Estilo padrão de altura
+  width: '', // Estilo padrão de largura
+  border: '', // Estilo padrão de borda
+  borderRadius: '', // Estilo padrão de raio de borda
+  fontFamily: '', // Estilo padrão de família de fonte
+  fontSize: '', // Estilo padrão de tamanho de fonte
+};
 
 const handleStyle = {
   element: btn,
@@ -47,6 +60,7 @@ function saveValues(name, value) {
   localStorage[name] = value;
 }
 
+
 function setValues() {
   const properties = Object.keys(localStorage); // array com keys
 
@@ -57,6 +71,24 @@ function setValues() {
   showCss();
 }
 setValues();
+
+function resetValues(event) {
+  event.preventDefault();
+  const properties = Object.keys(localStorage); // array com keys
+
+  properties.forEach(prop => {
+    localStorage.removeItem(prop);
+    controles.elements[prop].value = '';
+  });
+
+  for (const style in defaultStyles) {
+    btn.style[style] = defaultStyles[style];
+  }
+
+  css.innerText = '';
+  btn.innerText = 'Clique';
+}
+
 
 function showCss() {
   css.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>')
